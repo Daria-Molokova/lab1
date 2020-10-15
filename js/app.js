1964b3f876f15b88260d382a4c97d7998bf57e2c
Daria Molokova <dariamolokova192@gmail.com>
2020-10-16 00:39:25 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+// 1 задание
+
+// const name = prompt('Введите своё имя');
+// const surname = prompt('Введите свою фамилию');
+
+// alert(`Ваше имя и фамилия - ${name} ${surname}`);
+
+
+// 2 задание
+
+// const yearNow = 2020;
+// const year = prompt('Введите свой год рождения');
+
+// alert(`Ваш возраст: ${yearNow-year}`);
+
+// 3 задание
+// const length = prompt('Введите длину квадрата');
+// alert(`Периметр: ${length*4}, площадь: ${length**2}`);
+
+// 4 задание
+
+// const radius = prompt('Введите радиус окружности');
+// alert(`Площадь: ${Math.PI*radius**2}`);
+
+// 5 задание
+
+// const length = prompt('Введите расстояние между городами');
+// const time = prompt('Введите время пути');
+// alert(`Необходимая скорость: ${length/time}`);
+
+// 6 задание
+// const dol = prompt('Введите сумму в долларах');
+
+// const euro = dol*0.85;
+// const gryvna = dol*28.36;
+
+// alert(`Доллары в евро: ${euro}, доллары в гривны: ${gryvna}`);
+
+// 7 задание
+
+// const valuem = prompt('Введите объём флешки');
+// const file = valuem*1000/820;
+// alert(`Количество файлов: ${Math.round(file)}`);
+
+//8 задание
+
+// const summa = prompt('Введите сумму денег в кошельке');
+// const price = prompt('Введите цену шоколадки');
+// const quantity = Math.floor(summa/price);
+
+// alert(`Количество шоколадок: ${quantity}, сумма сдачи: ${summa-price*quantity}`);
+
+// 9 задание
+
+// const num = prompt('Введите трёхзначное число');
+
+// alert(`Число задом наперёд: ${num % 10}${num[1]}${num[0]}`);
